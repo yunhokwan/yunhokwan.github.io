70f1fab4fb99b20619c933a5d3d8ff3ac1ccc049
yunhokwan <43600252+yunhokwan@users.noreply.github.com>
2018-10-13 11:03:27 +0800

    upload file

---
+++ Assignment 2/Calculator.js
@@ -0,0 +1,38 @@
+function addition(a, b)
+{ a = parseInt(a); b = parseInt(b);
+c = a + b;
+return c; }
+function get_sumvalues() {
+var a = prompt(“Enter first number:”);
+var b = prompt(“Enter second number:”);
+var d = addition(a,b); alert(“The answer is:” + d }
+
+function subtraction(e, f)
+{ e = parseInt(e); f = parseInt(f);
+g = e - f;
+return h; }
+function get_differencevalues() {
+var e = prompt(“Enter first number:”);
+var f = prompt(“Enter second number:”);
+var g = subtraction(e,f); alert(“The answer is:” + g);
+}
+
+function multiplication(i, j)
+{ i = parseInt(i); j = parseInt(j);
+k = i * j;
+return l;}
+function get_multiplicationvalues() {
+var i = prompt(“Enter first number:”);
+var j = prompt(“Enter second number:”);
+var k = multiplication(i,j); alert(“The answer is:” + k);
+}
+
+function division(m, n)
+{ m = parseInt(m); n = parseInt(n);
+o = m / n;
+return p; }
+function get_divisionvalues() {
+var m = prompt(“Enter first number:”);
+var n = prompt(“Enter second number:”);
+var o = division(m,n); alert(“The answer is:” + o);
+}
